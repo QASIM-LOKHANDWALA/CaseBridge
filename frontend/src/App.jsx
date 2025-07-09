bbe7932590b16e3838de0ce6c037c15890dd2013
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2025-07-09 15:25:21 +0530

    Design Landing Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import LandingPage from "./pages/LandingPage";
 
 const App = () => {
-    return <div>Hello</div>;
+    return (
+        <div>
+            <LandingPage />
+        </div>
+    );
 };
 
 export default App;
