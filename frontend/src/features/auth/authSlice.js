39f54d94522a07fcf4e3ce8689468e28c59672b4
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2025-07-10 17:28:26 +0530

    Setup Redux And Create Auth Slice

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,164 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { act } from "react";
+
+const loginUser = createAsyncThunk(
+    "auth/loginUser",
+    async (userData, { rejectWithValue }) => {
+        try {
+            const response = await axios.post(
+                "http://localhost:8000/user/login/",
+                userData,
+                {
+                    headers: {
+                        "Content-Type": "application/json,",
+                    },
+                }
+            );
+            if (response.status === 200) {
+                const { token, user } = response.data;
+                localStorage.setItem("token", token);
+                return { user, token };
+            }
+
+            return rejectWithValue("Login failed");
+        } catch (error) {
+            if (error.response && error.response.data) {
+                return rejectWithValue(
+                    error.response.data.message || "Server error"
+                );
+            }
+            return rejectWithValue(error.message || "Network error");
+        }
+    }
+);
+
+const signUpUser = createAsyncThunk(
+    "auth/signUpUser",
+    async (userData, { rejectWithValue }) => {
+        try {
+            const response = await axios.post(
+                "http://localhost:8000/user/signup/",
+                userData,
+                {
+                    headers: {
+                        "Content-Type": "application/json,",
+                    },
+                }
+            );
+            if (response.status === 201) {
+                const { user } = response.data;
+                localStorage.setItem("token", token);
+                return { user };
+            }
+
+            return rejectWithValue("Sign up failed");
+        } catch (error) {
+            if (error.response && error.response.data) {
+                return rejectWithValue(
+                    error.response.data.message || "Server error"
+                );
+            }
+            return rejectWithValue(error.message || "Network error");
+        }
+    }
+);
+
+export const logoutUser = createAsyncThunk(
+    "auth/logoutUser",
+    async (_, { rejectWithValue, getState }) => {
+        try {
+            const token = getState().auth.token;
+
+            const response = await fetch("http://127.0.0.1:8000/user/logout", {
+                method: "GET",
+                headers: {
+                    Authorization: `Token ${token}`,
+                },
+            });
+            if (response.ok) {
+                localStorage.removeItem("token");
+                return { message: "Sign out successfully" };
+            }
+            return rejectWithValue("Sign out failed");
+        } catch (error) {
+            if (error.response && error.response.data) {
+                return rejectWithValue(
+                    error.response.data.message || "Server error"
+                );
+            }
+            return rejectWithValue(error.message || "Network error");
+        }
+    }
+);
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState: {
+        user: null,
+        isAuthenticated: false,
+        loading: false,
+        error: null,
+        token: null,
+    },
+    reducers: {},
+
+    extraReducers: (builder) => {
+        builder
+            .addCase(loginUser.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(loginUser.fulfilled, (state, action) => {
+                state.loading = false;
+                state.isAuthenticated = true;
+                state.user = action.payload.user;
+                state.token = action.payload.token;
+            })
+            .addCase(loginUser.rejected, (state, action) => {
+                state.loading = false;
+                state.isAuthenticated = false;
+                state.user = null;
+                state.token = null;
+                state.error = action.payload || "Login failed";
+            })
+
+            .addCase(signUpUser.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(signUpUser.fulfilled, (state, action) => {
+                console.log("Signup fulfilled:", action.payload);
+                state.loading = false;
+                state.user = action.payload.user;
+                state.token = null;
+                state.isAuthenticated = false;
+                state.error = null;
+            })
+            .addCase(signUpUser.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload || "Sign up failed";
+            })
+
+            .addCase(logoutUser.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(logoutUser.fulfilled, (state) => {
+                console.log("Signout fulfilled");
+                state.loading = false;
+                state.user = null;
+                state.token = null;
+                state.isAuthenticated = false;
+                state.error = null;
+            })
+            .addCase(logoutUser.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            });
+    },
+});
+
+export const {} = authSlice.actions;
+
+export default authSlice.reducer;
